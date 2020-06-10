d0c835a83e7ac44607fb4e4888225cb2aa0739d8
Marine Hambaryan <marinehambaryan@Marines-MacBook-Pro.local>
2020-06-10 16:27:52 +0400

    message shown if no countries, minor changes in search input container

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -13,7 +13,16 @@ function HomePage(props) {
         changeSelectedFilter={props.changeSelectedFilter}
         handleInputChange={props.handleInputChange}
       />
-      <Elements data={props.data} />
+      {props.data.length > 0 ? (
+        <Elements data={props.data} />
+      ) : (
+        <div className="messageContainer">
+          <p>
+            No Countries with a name <b>{props.searchInputValue}</b> and{" "}
+            <b>{props.selectedFilter}</b> region
+          </p>
+        </div>
+      )}
     </div>
   );
 }
